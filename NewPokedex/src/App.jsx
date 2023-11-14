0ebe2ccaa31c2c038844068bd610a75fdefc6d0f
Bravax <xavierlobez@hotmail.fr>
2023-11-14 22:01:53 +0100

    Challenge my first component

---
+++ NewPokedex/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import PokemonCard from './components/PokemonCard';
+import './components/PokemonCard.css';
+
+function App() {
+
+
+  return (
+    <>
+      <PokemonCard />
+    </>
+  )
+}
+
+export default App
---
+++ NewPokedex/src/components/PokemonCard.jsx
@@ -0,0 +1,16 @@
+function PokemonCard() {
+
+    return (
+        <>
+            <figure className="avatar">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemon bulbasaur" className="cardImg"/>
+
+                <figcaption>Bulbasaur</figcaption>
+            </figure>
+        </>
+    );
+
+}
+
+export default PokemonCard;
+
